207fa3169810ed38abb3cd25790eb169a4b3798a
Khaled Briki <81531221+Bril3d@users.noreply.github.com>
2022-08-24 13:55:35 +0100

    Improving Js Code

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
-(function () {
+
     const second = 1000,
           minute = second * 60,
           hour = minute * 60,
           day = hour * 24;
 
-    let today = new Date(),
-        dd = String(today.getDate()).padStart(2, "0"),
-        mm = String(today.getMonth() + 1).padStart(2, "0"),
-        yyyy = today.getFullYear(),
-        nextYear = yyyy + 1,
-        dayMonth = "09/30/",
-        birthday = dayMonth + yyyy;
+    let myBirthday = new Date("Jan, 03 2023 23:59:59").getTime();
     
-    today = mm + "/" + dd + "/" + yyyy;
-    if (today > birthday) {
-      birthday = dayMonth + nextYear;
-    }
     
-    const countDown = new Date(birthday).getTime(),
-        x = setInterval(function() {    
+        x = setInterval(() => {    
   
           const now = new Date().getTime(),
-                distance = countDown - now;
+                diff = myBirthday - now;
   
-            document.querySelector(".days").innerText = Math.floor(distance / (day)),
-            document.querySelector(".hours").innerText = Math.floor((distance % (day)) / (hour)),
-            document.querySelector(".minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-            document.querySelector(".seconds").innerText = Math.floor((distance % (minute)) / second);
+            let seconds = Math.floor((diff % (minute)) / second),
+                min = Math.floor((diff % (hour)) / (minute)),
+                hours = Math.floor((diff % (day)) / (hour)),
+                days = Math.floor(diff / (day));
+  
+                document.querySelector(".days").innerText = days < 10 ? `0${days}` : days;
+                document.querySelector(".hours").innerText = hours < 10 ? `0${hours}` : hours;
+                document.querySelector(".minutes").innerText = min < 10 ? `0${min}` : min;
+                document.querySelector(".seconds").innerText = seconds < 10 ? `0${seconds}` : seconds;
   
           
-          //seconds
+
         }, 0)
-    }());
